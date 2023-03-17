22aa1db1f2d269b64504a23007297f7004957776
vkasyan76 <vkasyan@gmx.de>
2023-03-17 16:35:13 +0100

    Tailwind configuration

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
-import Navbar from '@/scenes/navbar'
+// import Navbar from '@/scenes/navbar'
 
 function App() {
-  return <div className="app">app</div>
+  return <div className="app bg-gray-20">app</div>
 }
 
 export default App
