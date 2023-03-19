bf4ea50750e710ab238280fa5939abe4db6d424d
vkasyan76 <vkasyan@gmx.de>
2023-03-19 23:40:31 +0100

    Navbar Part 1: structure set-up

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
-// import Navbar from '@/scenes/navbar'
+import Navbar from '@/scenes/navbar'
 
 function App() {
-  return <div className="app bg-gray-20">app</div>
+  return (
+    <div className="app bg-gray-20">
+      <Navbar />
+    </div>
+  )
 }
 
 export default App
