ce57d8a977481e13ba2182b11b71d87be3fd89d0
vkasyan76 <vkasyan@gmx.de>
2023-03-27 21:08:02 +0200

    Navbar Part 2_React Anchor_Link component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
-import Navbar from '@/scenes/navbar'
+import Navbar from "@/scenes/navbar";
+import { useState } from "react";
 
-function App() {
-  return (
-    <div className="app bg-gray-20">
-      <Navbar />
-    </div>
-  )
+enum SelectedPage {
+  Home = "home",
+  Benefits = "benefits",
 }
 
-export default App
+function App() {
+  const [selectedPage, setSelectedPage] = useState<string>("home");
+
+  return (
+    <div className="app bg-gray-20">
+      <Navbar selectedPage={selectedPage} setSelectedPage={setSelectedPage} />
+    </div>
+  );
+}
+
+export default App;
--- src/scenes/navbar/index.tsx
+++ src/scenes/navbar/index.tsx
@@ -1,11 +1,15 @@
-import { useState } from 'react'
-import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/solid'
-import Logo from '@/assets/Logo.png'
+import { useState } from "react";
+import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/solid";
+import Logo from "@/assets/Logo.png";
+import Link from "./Link";
 
-type Props = {}
+type Props = {
+  selectedPage: string;
+  setSelectedPage: (value: string) => void;
+};
 
-const Navbar = (props: Props) => {
-  const flexBetween = 'flex items-center justify-between'
+const Navbar = ({ selectedPage, setSelectedPage }: Props) => {
+  const flexBetween = "flex items-center justify-between";
   return (
     <nav>
       <div className={`${flexBetween} fixed top-0 z-30 w-full py-6`}>
@@ -17,10 +21,31 @@ const Navbar = (props: Props) => {
             <div className={`${flexBetween} w-full`}>
               {/* INNER LEFT SIDE */}
               <div className={`${flexBetween} gap-8 text-sm`}>
-                <p>Home</p>
-                <p>Benefits</p>
-                <p>Our Classes</p>
-                <p>Contact Us</p>
+                <Link
+                  page="Home"
+                  selectedPage={selectedPage}
+                  setSelectedPage={setSelectedPage}
+                />
+                <Link
+                  page="Benefits"
+                  selectedPage={selectedPage}
+                  setSelectedPage={setSelectedPage}
+                />
+                <Link
+                  page="Our Classes"
+                  selectedPage={selectedPage}
+                  setSelectedPage={setSelectedPage}
+                />
+                <Link
+                  page="Our Classes"
+                  selectedPage={selectedPage}
+                  setSelectedPage={setSelectedPage}
+                />
+                <Link
+                  page="Contact Us"
+                  selectedPage={selectedPage}
+                  setSelectedPage={setSelectedPage}
+                />
               </div>
               {/* INNER RIGHT SIDE */}
               <div className={`${flexBetween} gap-8`}>
@@ -32,7 +57,7 @@ const Navbar = (props: Props) => {
         </div>
       </div>
     </nav>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
