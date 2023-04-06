4ed1d5694c93700a4067debcb04bf2218dd6b8cf
vkasyan76 <vkasyan@gmx.de>
2023-04-06 13:52:17 +0200

    Our Classes Page

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { useEffect, useState } from "react";
 import { SelectedPage } from "@/shared/types";
 import Home from "@/scenes/home";
 import Benefits from "./scenes/benefits";
+import OurClasses from "./scenes/ourClasses";
 
 function App() {
   const [selectedPage, setSelectedPage] = useState<SelectedPage>(
@@ -33,6 +34,7 @@ function App() {
       />
       <Home setSelectedPage={setSelectedPage} />
       <Benefits setSelectedPage={setSelectedPage} />
+      <OurClasses setSelectedPage={setSelectedPage} />
     </div>
   );
 }
